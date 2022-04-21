054560dcc488c341cc77847a9d47d0ef9dbf0be6
ahmadabd <ahmadabd13741112@gmail.com>
2022-04-21 12:15:30 +0430

    document service unitTests completed

--- internal/service/document/document_test.go
+++ internal/service/document/document_test.go
@@ -3,12 +3,14 @@ package document_test
 import (
 	"context"
 	"log"
+	"notary-public-online/internal/entity/model"
 	"notary-public-online/internal/service/document"
 	"notary-public-online/mocks"
 	"os"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 var mockDB *mocks.MockDB
@@ -47,7 +49,38 @@ func TestStoreDocument(t *testing.T) {
 
 	err := doc.StoreDocument(context.TODO(), file, "doc name", "doc description", 1)
 
-	if err != nil {
-		t.Errorf("error should be nil, got %v", err)
-	}
+	assert.Nil(t, err)
+}
+
+func TestDocumentDetails(t *testing.T) {
+	teardownSuite := setupSuite(t)
+	defer teardownSuite(t)
+
+	doc := document.New(mockDB, mockStorage)
+
+	mockDB.EXPECT().GetDocument(gomock.Any(), 1).Return(&model.Document{}, nil).Times(1)
+
+	_, err := doc.DocumentDetails(context.TODO(), 1)
+
+	assert.Nil(t, err)
+}
+
+func TestReadDocument(t *testing.T) {
+	teardownSuite := setupSuite(t)
+	defer teardownSuite(t)
+
+	doc := document.New(mockDB, mockStorage)
+
+	// store new Document
+	file, _ := os.Open("../../../mocks/fakeFile.txt")
+	mockDB.EXPECT().CreateDocument(gomock.Any(), "doc name", "doc description", "fakeFile.txt", gomock.Any(), 1, false).Return(nil).Times(1)
+	mockStorage.EXPECT().StoreFile(file).Return("fakeFile.txt", nil).Times(1)
+	doc.StoreDocument(context.TODO(), file, "doc name", "doc description", 1)
+
+	// Read Document
+	mockDB.EXPECT().GetDocumentAddress(gomock.Any(), 1).Return("fakeFile.txt", nil).Times(1)
+	mockStorage.EXPECT().ReadFile(gomock.Any()).Return(file, nil).Times(1)
+
+	_, err := doc.ReadDocument(context.TODO(), 1)
+	assert.Nil(t, err)
 }
